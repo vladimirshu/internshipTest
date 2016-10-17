50c79e275381d7d028d793dca2dbbae8be45be92
Vladimir Shumakov <v.shumakov@sam-solutions.com>
2016-10-17 10:05:29 +0300

    maven webapp

---
+++ src/main/java/com/test/internshipTest/Main.java
@@ -0,0 +1,29 @@
+/**
+ * ***********************************************************************
+ * Copyright (c) 2015, Russian Electronic Commerce (REC) <info@rec.expert>
+ *
+ * All portions of the code written by REC are property of REC.
+ * All Rights Reserved.
+ *
+ * Russian Electronic Commerce
+ * Kirochnaya st. 19, 191123
+ * Saint Petersburg, Russian Federation
+ * Tel: +7 (812) 385 59 79
+ * Web: rec.expert
+ * ***********************************************************************
+ */
+package com.test.internshipTest;
+
+/**
+ * Main.
+ *
+ * @author Vladimir Shumakov <v.shumakov@sam-solutions.com>
+ * @package com.test.internshipTest
+ */
+public class Main
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
